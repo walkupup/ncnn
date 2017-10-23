32f2394db89b381aadca7bf2264fcbfb32e42ace
yl <yl99@163.com>
2017-10-23 20:01:00 +0800

    add

---
+++ buildvs15/x64/Release/pd-model/caffe2ncnn.bat
@@ -0,0 +1,4 @@
+caffe2ncnn det1.prototxt ohsm1_det1_85000.caffemodel det1.param det1.bin
+caffe2ncnn det2.prototxt _iter_20000.caffemodel det2.param det2.bin
+caffe2ncnn det3.prototxt _iter_30000.caffemodel det3.param det3.bin
+pause
